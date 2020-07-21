aeaaf7ea7510e9ea7904703941c4da214a9b38fa
Stephen Brundage <stephen.brundage12@gmail.com>
2020-07-21 06:58:26 -0400

    Landing Page Complete
    - Landing.js completed
    - SocialBar updated to display text when hovering over
    - SocialBar links updated
    
    Landing section has been completed.  Most work was done on SocialBar component.  SocialBar items display text about what they are when they are hovered over.  Still need to handle scaling SocialBar for smaller screen sizes.  SocialBar items seem too large on mobile.

--- src/components/SocialBar.js
+++ src/components/SocialBar.js
@@ -6,20 +6,40 @@ export default function SocialBar() {
     return (
         <div className="social-container container">
             <a style={{zIndex: 1000}} href="https://www.linkedin.com/in/stevebrundage">
-                <i class="fab fa-linkedin bar-item" ></i>
+                <div class="bar-item-container">
+                    <i class="fab fa-linkedin bar-item">
+                        <p>LinkedIn</p>
+                    </i>
+                </div>
             </a>
             <a style={{zIndex: 1000}} href="https://github.com/sbrundage">
-                <i class="fab fa-github-alt bar-item"></i>
+                <div class="bar-item-container">
+                    <i class="fab fa-github-alt bar-item">
+                        <p>Github</p>
+                    </i>
+                </div>
             </a>
             <a style={{zIndex: 1000}} href="https://www.SteveBrundage.com/blog">
-                <i class="far fa-keyboard bar-item"></i>
+                <div class="bar-item-container">
+                    <i class="far fa-keyboard bar-item">
+                        <p>Blog</p>
+                    </i>
+                </div>
             </a>
             <a style={{zIndex: 1000}} href="https://twitter.com/SteveKBrundage">
-                <i class="fa fa-twitter bar-item"></i>
+                <div class="bar-item-container">
+                    <i class="fa fa-twitter bar-item">
+                        <p>Twitter</p>
+                    </i>
+                </div>
             </a>
-            {/* <a style={{zIndex: 1000}} href=""> */}
-                <i class="fas fa-id-card bar-item"></i>
-            {/* </a> */}
+            <a style={{zIndex: 1000}} href="">
+                <div class="bar-item-container">
+                    <i class="fas fa-id-card bar-item">
+                        <p>Resume</p>
+                    </i>
+                </div>
+            </a>
         </div>
     );
 }
