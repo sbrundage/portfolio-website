9f041a8df49e7f0992abf397aebe8b542f7b202f
Steve Brundage <brundage12@gmail.com>
2026-05-05 23:14:17 -0400

    Complete Refactor
    Clean up and refactor entire view to make it simple.

--- src/components/SocialBar.js
+++ src/components/SocialBar.js
@@ -1,39 +1,49 @@
 import React from 'react';
 
-import Resume from '../assets/Stephen_Brundage_Resume.pdf'
+import Resume from '../assets/Stephen_Brundage_Resume.pdf';
+import { LinkedInIcon, GitHubIcon, ResumeIcon, BlogIcon } from './Icons.js';
 import './SocialBar.css';
 
+const links = [
+  {
+    href: 'https://www.linkedin.com/in/stevebrundage',
+    label: 'LinkedIn',
+    Icon: LinkedInIcon,
+  },
+  {
+    href: 'https://github.com/sbrundage',
+    label: 'GitHub',
+    Icon: GitHubIcon,
+  },
+  {
+    href: 'https://www.SteveBrundage.com/blog',
+    label: 'Blog',
+    Icon: BlogIcon,
+  },
+  {
+    href: Resume,
+    label: 'Resume',
+    Icon: ResumeIcon,
+    download: true,
+  },
+];
+
 export default function SocialBar() {
-    return (
-        <div className="social-container container">
-            <a style={{zIndex: 1000}} href="https://www.linkedin.com/in/stevebrundage">
-                <div class="bar-item-container">
-                    <i class="fab fa-linkedin bar-item">
-                        <p>LinkedIn</p>
-                    </i>
-                </div>
-            </a>
-            <a style={{zIndex: 1000}} href="https://github.com/sbrundage">
-                <div class="bar-item-container">
-                    <i class="fab fa-github-alt bar-item">
-                        <p>Github</p>
-                    </i>
-                </div>
-            </a>
-            {/* <a style={{zIndex: 1000}} href="https://www.SteveBrundage.com/blog">
-                <div class="bar-item-container">
-                    <i class="far fa-keyboard bar-item">
-                        <p>Blog</p>
-                    </i>
-                </div>
-            </a> */}
-            <a style={{zIndex: 1000}} href={Resume} download>
-                <div class="bar-item-container">
-                    <i class="fas fa-id-card bar-item">
-                        <p>Resume</p>
-                    </i>
-                </div>
-            </a>
-        </div>
-    );
+  return (
+    <nav className="social-bar" aria-label="Social and contact links">
+      {links.map(({ href, label, Icon, download }) => (
+        <a
+          key={label}
+          href={href}
+          className="bar-item"
+          target={download ? undefined : '_blank'}
+          rel={download ? undefined : 'noopener noreferrer'}
+          download={download || undefined}
+        >
+          <Icon />
+          <span className="bar-item-label">{label}</span>
+        </a>
+      ))}
+    </nav>
+  );
 }
